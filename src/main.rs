c6cd49f61cce570fb95b2b24dea2f9b6a9601d09
itsgerliz <gerworks.hs@gmail.com>
2024-11-29 12:22:37 +0100

    feat!(main.rs): Hexreader works

--- src/main.rs
+++ src/main.rs
@@ -10,21 +10,45 @@ struct CliArgs {
 	files: Vec<String>
 }
 
-fn read_hex(c_file: &File) {
-	
+fn read_hex(c_file: &mut File) {
+	let mut buffer: [u8; 8] = [0; 8];
+	loop {
+		match c_file.read(&mut buffer) {
+			Ok(0) => {
+				println!();
+				println!("Reached EOF");
+				break;
+			}
+			Ok(n) => {
+				let mut counter = 0;
+				for i in &buffer[0..n] {
+					print!("{:02X} ", i);
+					counter += 1; // (0)->1->2->3->4->5->6->7->8
+					counter = counter % 8; // 8 % 8 = 0
+					if counter == 0 {
+						println!();
+					}
+				}
+			}
+			Err(e) => {
+				eprintln!("{}", e);
+				break;
+			}
+		}
+	}
 }
 
 fn main() {
 	let parsed = CliArgs::parse();
 
 	for i in parsed.files {
-		let current = match File::open(&i) {
+		let mut current = match File::open(&i) {
 			Ok(c) => { c }
 			Err(e) => {
 				eprintln!("{}", e);
 				exit(1);
 			}
 		};
-		read_hex(&current);
+		read_hex(&mut current);
 	}
 }
